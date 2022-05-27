f2dc41c0100ec86e8f08b5353603b478af089d7b
Ilya Barinov <ilia.barinov2012@gmail.com>
2022-05-28 03:01:05 +0500

    fix

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    secretAccess: "SECRET_KEY_RANDOM_ACCESS",
+    secretRefresh: "SECRET_KEY_RANDOM_REFRESH",
+    secretRecovery: "SECRET_KEY_RANDOM_RECOVERY",
+    email_pass: "thirdpoint"
+}
